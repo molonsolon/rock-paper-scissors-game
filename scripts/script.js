4570cefb9b5dc305c01101abe0a3a4a0ce8199f5
Joseph Coulter <joeycoulter@gmail.com>
2021-08-07 13:47:55 -0700

    Finalized functionality, basic gui

--- scripts/script.js
+++ scripts/script.js
@@ -16,21 +16,81 @@ const paper = document.querySelector(`#paper`);
 const scissors = document.querySelector(`#scissors`);
 const results = document.querySelector(`#results`);
 const gameChoice = document.getElementsByClassName(`.game-choice`);
- 
+const score = document.querySelector(`#score`);
+
+
+let playerScore = 0;
+let computerScore = 0;
+
 rock.addEventListener(`click`, () => {
-    let playerScore = 0;
-    let computerScore = 0;
     results.textContent = playRound(`rock`);
     
+    if (playerScore === 5) {
+        score.textContent = `Final Score -- Your score: ${playerScore}.
+        Computer score: ${computerScore}`;
+        alert(`You win! Let's go again.`);
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore === 5) {
+        score.textContent = `Final Score -- Your score: ${playerScore}.
+        Computer score: ${computerScore}`;
+        alert(`You lose :( Try again!`);
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else {
+        score.textContent = `Your score: ${playerScore}.
+        Computer score: ${computerScore}`;
+    }
 });
 
 paper.addEventListener(`click`, () => {
     results.textContent = playRound(`paper`);
+    
+    if (playerScore === 5) {
+        score.textContent = `Final Score -- Your score: ${playerScore}.
+        Computer score: ${computerScore}`;
+        alert(`You win! Let's go again.`);
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore === 5) {
+        score.textContent = `Final Score -- Your score: ${playerScore}.
+        Computer score: ${computerScore}`;
+        alert(`You lose :( Try again!`);
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else {
+        score.textContent = `Your score: ${playerScore}.
+        Computer score: ${computerScore}`;
+    }
 });
 
 scissors.addEventListener(`click`, () => {
     results.textContent = playRound(`scissors`);
+    
+    if (playerScore === 5) {
+        score.textContent = `Final Score -- Your score: ${playerScore}.
+        Computer score: ${computerScore}`;
+        alert(`You win! Let's go again.`);
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore === 5) {
+        score.textContent = `Final Score -- Your score: ${playerScore}.
+        Computer score: ${computerScore}`;
+        alert(`You lose :( Try again!`);
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else {
+        score.textContent = `Your score: ${playerScore}.
+        Computer score: ${computerScore}`;
+    }
 });
+
 
 
 // ** write psuedo-code for this, getting too lost in the weeds
@@ -48,36 +108,45 @@ scissors.addEventListener(`click`, () => {
 //});
 
 
+
+
+
 //individual round of R,P,S used within game() 
 function playRound(playerSelection) {
     
     const computerSelection = computerPlay();
     
+
+    
     //string comparisons to declare winner
     if  ((playerSelection.toLowerCase() ===  `rock`) & (computerSelection === `Scissors`)) {
+        playerScore++;
         return `Hooray! Rock smashes scissors. You win!`; 
     } else if ((playerSelection.toLowerCase() ===  `rock`) & (computerSelection === `Paper`)) {
+        computerScore++;
         return `Damn... Paper smothers rock. You lose.`;
-            
    } else if ((playerSelection.toLowerCase() ===  `paper`) & (computerSelection === `Rock`)) {
+        playerScore++;
         return `Dope! Paper beats rock. You win!`;
-
    } else if ((playerSelection.toLowerCase() ===  `paper`) & (computerSelection === `Scissors`)) {
+        computerScore++
         return `Shoot! Scissors cut paper. You lose.`;
-
     } else if ((playerSelection.toLowerCase() ===  `scissors`) & (computerSelection === `Paper`)) {
+        playerScore++;
         return `Yeeeaaaaaaa boiiiii, scissors slice paper. You win!`;
-
     } else if ((playerSelection.toLowerCase() ===  `scissors`) & (computerSelection === `Rock`)) {
+        computerScore++
         return `Demolished!! Rock smashes scissors. You lose.`;
-
     } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
         return `Wait a second... It's a tie! try again <3`;
-
     } else { 
         return `something has gone wrong`;
         }
+    
+
+    
 }
+
 
 
 
