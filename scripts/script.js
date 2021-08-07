c69abfa9ee30ec776d04d2313d7a0e3685dc94f1
Joseph Coulter <joeycoulter@gmail.com>
2021-08-07 15:24:44 -0700

    cleaning

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
 //Rock, Paper, Scissors game for The Odin Project
 
+//Queries
+const rock = document.querySelector(`#rock`);
+const paper = document.querySelector(`#paper`);
+const scissors = document.querySelector(`#scissors`);
+const results = document.querySelector(`#results`);
+const gameChoice = document.getElementsByClassName(`.game-choice`);
+const score = document.querySelector(`#score`);
 
 //computer's random array choice
 function computerPlay() {
@@ -11,31 +18,28 @@ function computerPlay() {
    return randomChoice
 }
 
-const rock = document.querySelector(`#rock`);
-const paper = document.querySelector(`#paper`);
-const scissors = document.querySelector(`#scissors`);
-const results = document.querySelector(`#results`);
-const gameChoice = document.getElementsByClassName(`.game-choice`);
-const score = document.querySelector(`#score`);
 
 
+// Starting scores
 let playerScore = 0;
 let computerScore = 0;
 
+//Event listeners for each decision
+//** investigate ways to combine these three functions. Must determine choice based on query selected.
 rock.addEventListener(`click`, () => {
     results.textContent = playRound(`rock`);
     
     if (playerScore === 5) {
         score.textContent = `Final Score -- Your score: ${playerScore}.
         Computer score: ${computerScore}`;
-        alert(`You win! Let's go again.`);
+        results.textContent += ` Select another choice to play again.`
         playerScore = 0;
         computerScore = 0;
     }
     else if (computerScore === 5) {
         score.textContent = `Final Score -- Your score: ${playerScore}.
         Computer score: ${computerScore}`;
-        alert(`You lose :( Try again!`);
+        results.textContent += ` Select another choice to play again.`;
         playerScore = 0;
         computerScore = 0;
     }
@@ -51,14 +55,14 @@ paper.addEventListener(`click`, () => {
     if (playerScore === 5) {
         score.textContent = `Final Score -- Your score: ${playerScore}.
         Computer score: ${computerScore}`;
-        alert(`You win! Let's go again.`);
+        results.textContent += ` Select another choice to play again.`
         playerScore = 0;
         computerScore = 0;
     }
     else if (computerScore === 5) {
         score.textContent = `Final Score -- Your score: ${playerScore}.
         Computer score: ${computerScore}`;
-        alert(`You lose :( Try again!`);
+        results.textContent += ` Select another choice to play again.`;
         playerScore = 0;
         computerScore = 0;
     }
@@ -74,14 +78,14 @@ scissors.addEventListener(`click`, () => {
     if (playerScore === 5) {
         score.textContent = `Final Score -- Your score: ${playerScore}.
         Computer score: ${computerScore}`;
-        alert(`You win! Let's go again.`);
+        results.textContent += ` Select another choice to play again.`
         playerScore = 0;
         computerScore = 0;
     }
     else if (computerScore === 5) {
         score.textContent = `Final Score -- Your score: ${playerScore}.
         Computer score: ${computerScore}`;
-        alert(`You lose :( Try again!`);
+        results.textContent += ` Select another choice to play again.`;
         playerScore = 0;
         computerScore = 0;
     }
@@ -93,32 +97,10 @@ scissors.addEventListener(`click`, () => {
 
 
 
-// ** write psuedo-code for this, getting too lost in the weeds
-// ** going by trial and error when i could be saving lots of time
-// ** reserch but don't just steal all code from s.o. answers
-
-// old code ideas... try some more maybe? need to have a function enter player selection
-// selected through recognizing id.
-//const choiceEntry = function() {
-//    const choice = this.getAttribute(`id`);
-//}
-
-//Array.from(gameChoice, e => { 
-//    e.addEventListener(`click`, playRound(choiceEntry))
-//});
-
-
-
-
-
-//individual round of R,P,S used within game() 
+//individual round of R,P,S
 function playRound(playerSelection) {
-    
     const computerSelection = computerPlay();
-    
 
-    
-    //string comparisons to declare winner
     if  ((playerSelection.toLowerCase() ===  `rock`) & (computerSelection === `Scissors`)) {
         playerScore++;
         return `Hooray! Rock smashes scissors. You win!`; 
@@ -141,47 +123,5 @@ function playRound(playerSelection) {
         return `Wait a second... It's a tie! try again <3`;
     } else { 
         return `something has gone wrong`;
-        }
-    
-
-    
-}
-
-
-
-
-//initiates a game of R,P,S that ends when both player's scores total 5. 
-function game() {
-   
-
-    //score variables
-    let playerScore = 0;
-    let computerScore = 0;
-    let result;
-    
-    //loop 5x
-    do {
-        alert(result =  playRound());
-    
-        if (result.includes("win")) {
-            playerScore++
-        } else if (result.includes("lose")) {
-            computerScore++
-        };
-    } while ((playerScore + computerScore) < 5);
-
-    console.log(computerScore);
-    console.log(playerScore);
-    
-    //winner alert
-    if (computerScore > playerScore) {
-       return alert("Looks like your luck has run out... try again.")
-    } else if (computerScore < playerScore) {
-       return alert("Wow! You got lucky this time...")
-    } else {
-       return alert("A tie?? Let's give it another go, shall we?")
     }
-    
-    
 }
-
